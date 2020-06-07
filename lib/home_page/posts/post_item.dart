a32b3e70938581e6f232e17def6a5d37971dac0c
santa112358 <sun.jp.0212@gmail.com>
2020-06-07 23:32:32 +0900

    build post outline

--- lib/home_page/posts/post_item.dart
+++ lib/home_page/posts/post_item.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_mock/components/circle_image.dart';
+import 'package:instagram_mock/constants.dart';
 
 class PostItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 300,
-      color: Colors.green,
-      child: Text("post"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Divider(
+          height: 1,
+          thickness: 1,
+        ),
+        SizedBox(
+          height: 64,
+          child: Row(
+            children: <Widget>[
+              const SizedBox(
+                width: 12,
+              ),
+              CircleImage(
+                imageUrl: jobsImage,
+                width: 48,
+                height: 48,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Steeve",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.width,
+          child: Image.network(
+            jobsImage,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/home_page/stories/story_item.dart
+++ lib/home_page/stories/story_item.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_mock/components/circle_image.dart';
 
 class StoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 68,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: Colors.green,
-          width: 2,
-        ),
-      ),
-      margin: EdgeInsets.symmetric(
-        horizontal: 12,
-      ),
-      padding: EdgeInsets.all(2),
-      child: Center(
-        child: ClipOval(
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Image.network(
-              "https://s3.amazonaws.com/production.mediajoint.prx.org/public/piece_images/421548/jobs_square.jpg",
-              fit: BoxFit.cover,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 68,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.green,
+              width: 2,
             ),
           ),
+          margin: EdgeInsets.symmetric(
+            horizontal: 12,
+          ),
+          padding: EdgeInsets.all(2),
+          child: CircleImage(
+            imageUrl:
+                "https://s3.amazonaws.com/production.mediajoint.prx.org/public/piece_images/421548/jobs_square.jpg",
+          ),
         ),
-      ),
+        Text(
+          "Steeve",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
