8c863aaee849e32bbd3f48a923e271bfec9da294
santa112358 <sun.jp.0212@gmail.com>
2020-06-07 23:08:26 +0900

    build story ui outline

--- lib/home_page/stories/story_item.dart
+++ lib/home_page/stories/story_item.dart
@@ -1,12 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 30,
-      width: 40,
-      color: Colors.red,
+      width: 68,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.green,
+          width: 2,
+        ),
+      ),
+      margin: EdgeInsets.symmetric(
+        horizontal: 12,
+      ),
+      padding: EdgeInsets.all(2),
+      child: Container(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
     );
   }
 }
